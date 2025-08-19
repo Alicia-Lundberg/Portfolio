6eca8c695fd7f8f4af62c685c7b62dfa39d73c6b
Alicia-Lundberg <alicia_lundberg@github.com>
2025-08-19 20:00:55 +0200

    bug

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -3,7 +3,13 @@ import { projects } from "@/data/projects";
 import ProjectCarousel from "./ProjectCarousel";
 
 
-export default function Page({ params }: any) {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Page({ params }: PageProps) {
   const { slug } = params;
   const project = projects.find((p) => p.slug === slug);
 
